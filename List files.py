51aaf934df5e7f4cfcfb3f36bfc69e968118d1eb
Jakub Białkowski <jakub.bialkowski@fi.abb.com>
2019-02-19 16:01:45 +0100

    porządeczek i funkcja listująca zawartosc sciezki

--- List files.py
+++ List files.py
@@ -1,13 +1,6 @@
 import os
 
 
-def list_current_path_files_and_directories(path, do_print):
-    directory_list = os.listdir(path)
-    if do_print:
-        print(directory_list)
-    return directory_list
-
-
 def get_current_path(do_print):
     current_path = os.getcwd()
     if do_print:
@@ -19,35 +12,28 @@ def change_directory(destination_path):
     os.chdir(destination_path)
 
 
-def create_current_directory_path_list():
-    dir_path_list = []
-    current_path = get_current_path(False)
-    dir_list = list_current_path_files_and_directories(current_path, False)
+# Function return dictionary ('dir', 'fil') that contain content of given path
 
-    for i in range(0, len(dir_list)):
-        dir_path = os.path.join(current_path, dir_list[i])
-        dir_path_list.append(dir_path)
-    return dir_path_list
-
-
-def split_list_into_directories_and_files(patch_list):
+def list_path_content(path_to_list):
     directories = []
     files = []
+    dir_list = os.listdir(path_to_list)
 
-    for patch_no in range (0, len(patch_list)):
-        if os.path.isdir(patch_list[patch_no]):
-            directories.append(patch_list[patch_no])
+    for i in range(0, len(dir_list)):
+        dir_path = os.path.join(path_to_list, dir_list[i])
+
+        if os.path.isdir(dir_path):
+            directories.append(dir_path)
         else:
-            files.append(patch_list[patch_no])
+            files.append(dir_path)
 
     split_patches = {
         "dir": directories,
         "fil": files
     }
-
     return split_patches
 
 
-ona = create_current_directory_path_list()
-eyn = split_list_into_directories_and_files(ona)
-print(eyn["fil"])
+ona = get_current_path(False)
+eyn = list_path_content(ona)
+print(eyn["dir"])

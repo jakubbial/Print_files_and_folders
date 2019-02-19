4f09ff9792a30b5b8f1eedb0be6015e25d7639ba
Jakub Białkowski <jakub.bialkowski@fi.abb.com>
2019-02-19 11:09:48 +0100

    zamieniona funkcja listowania ściezek

--- List files.py
+++ List files.py
@@ -19,16 +19,16 @@ def change_directory(destination_path):
     os.chdir(destination_path)
 
 
-def create_directories_path(current_path, dir_list):
+def create_current_directory_path_list():
     dir_path_list = []
+    current_path = get_current_path(True)
+    dir_list = list_current_path_files_and_directories(current_path, False)
+
     for i in range(0, len(dir_list)):
         dir_path = os.path.join(current_path, dir_list[i])
         dir_path_list.append(dir_path)
     return dir_path_list
 
 
-curr_path = get_current_path(True)
-fil_dir_list = list_current_path_files_and_directories(curr_path, True)
-
-ona = create_directories_path(curr_path, fil_dir_list)
+ona = create_current_directory_path_list()
 print(ona)

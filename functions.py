eb44515da86ca639791071f4c1bb38c0598aa18f
Jakub Białkowski <jakub.bialkowski@fi.abb.com>
2019-02-22 11:18:02 +0100

    jeden plik skryptowy

--- functions.py
+++ functions.py
@@ -10,19 +10,12 @@ def get_current_path(do_print):
     return current_path
 
 
-# Function that changing current working directory
-
-def change_directory(destination_path):
-    os.chdir(destination_path)
-
-
 # Function return dictionary ('dir', 'fil') that contain content of given path
 
 def list_path_content(path_to_list):
     directories = []
     files = []
     directory_content_list = os.listdir(path_to_list)
-#    directories.append(path_to_list)
 
     for i in range(0, len(directory_content_list)):
         dir_path = os.path.join(path_to_list, directory_content_list[i])
@@ -37,8 +30,18 @@ def list_path_content(path_to_list):
     return split_patches
 
 
-#aktualna_sciezka = get_current_path(False)
-#zawartosc_slownik = list_path_content(aktualna_sciezka)
+def list_content_of_all_path_folders(path, i):
+    if i == len(path):
+        return path
 
-#print(zawartosc_slownik["dir"])
-#print(zawartosc_slownik["fil"])
+    path += list_path_content(path[i])
+    i += 1
+    return list_content_of_all_path_folders(path, i)
+
+
+# pat = [get_current_path(False)]
+pat = ["C:\ABB_repos\PanelTestAutomation\TestCases\AutomaticTests\ACX580"]
+
+
+oto = list_content_of_all_path_folders(pat, 0)
+print(oto)
